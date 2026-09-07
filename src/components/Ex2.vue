2d27e1b84ac8c7666c64a66cf58d5ba90745ac34
JJyayy <jkjanani66@gmail.com>
2026-09-07 09:21:00 +0800

    message

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -1,9 +1,23 @@
 <script setup>
 import { ref } from 'vue'
-
+import me from '/assets/me.png'
 // Add code here
+const width= ref('200')
+const height= ref ('200')
+const myImg= ref ('/assests/me.png')
+
+const obj=ref({
+  width: '200',
+  height: '200',
+  src: '/assests/me.png'
+})
 </script>
 
 <template>
   <!-- Add code here -->
+   
+  <img v-bind:src="myImg" :width="width" :height="height"/>
+  <img v-bind="obj">
+  
+
 </template>
