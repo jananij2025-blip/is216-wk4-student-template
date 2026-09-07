bd3d8259dfff45cea25d7f95111605c1b11942a3
JJyayy <jkjanani66@gmail.com>
2026-09-07 09:52:27 +0800

    message

--- src/components/Ex4.vue
+++ src/components/Ex4.vue
@@ -39,14 +39,24 @@ function changeTextColor() {
   <!-- Add/Modify code here -->
   <!-- note: need to use single quotes '' for classes such as btn-primary which contains '-' signs
                 because '-' is a minus operator for (Vue) JavaScript  -->
-  <div id="part1">
-    <div>div ID : {{ id }}</div>
+   <div id="part1">
+    <div v-bind:id="id" v-bind:class="{ blueBox: blueBox, redBox: !blueBox }">
+      div ID : {{ id }}
+    </div>
 
-    <button type="button" v-on:click="changeColor">Change Color</button>
+    <button
+      type="button"
+      v-on:click="changeColor"
+      v-bind:class="{ 'btn-primary': blueBtn, 'btn-danger': !blueBtn }"
+    >
+      Change Color
+    </button>
   </div>
 
   <div id="part2">
-    <div>div ID : {{ id }}</div>
+    <div v-bind:style="{ color: activeColor }">
+      div ID : {{ id }}
+    </div>
 
     <button type="button" v-on:click="changeTextColor">Change Text Color</button>
   </div>
